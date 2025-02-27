baccfc9924cf608170f7cdc638d8e0d2bdf9a398
jhon-ace <justcodingph.97@gmail.com>
2025-02-27 12:20:56 +0800

    tofix

--- resources/views/Sao/index.blade.php
+++ resources/views/Sao/index.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     @if (Auth::user()->hasRole('admin'))
-        <x-user-route-page-name :routeName="'admin.attendance.holiday'" />
+        <!-- <x-user-route-page-name :routeName="'admin.attendance.holiday'" /> -->
     @elseif(Auth::user()->hasRole('admin_staff'))
-        <x-user-route-page-name :routeName="'admin_staff.attendance.holiday'" />
+        <!-- <x-user-route-page-name :routeName="'admin_staff.attendance.holiday'" /> -->
     @elseif(Auth::user()->hasRole('sao'))
         <x-user-route-page-name :routeName="'sao.dashboard'" />
     @else
-        <x-user-route-page-name :routeName="'staff.attendance.holiday'" />
+        <!-- <x-user-route-page-name :routeName="'staff.attendance.holiday'" /> -->
     @endif
         <x-content-design>
             <!-- Content Area -->
@@ -18,7 +18,8 @@
                 <div class="relative">
                     <div class="container shadow-lg p-5 sm:p-6 md:p-7 lg:p-8 bg-white rounded-md text-black font-medium"
                         :style="{ 'width': isFullScreen ? 'calc(100vw - 16px)' : 'auto', 'margin-left': isFullScreen ? '-192px' : '0' }">
-                        <livewire:admin.show-student-attendance />
+                       <h1>Hellooooo</h1>
+                       
                     </div>
                 </div>
             </div>
